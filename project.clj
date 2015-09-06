f4460a7c7069570ebde87505454ac0cf41a87fc2
wuzhenzhou <greatzzwu@gmail.com>
2015-09-06 16:42:34 +0800

    add url in project.clj

--- project.clj
+++ project.clj
@@ -1,5 +1,6 @@
 (defproject ring-jdbc-single-session "0.0.1"
   :description "用户单一的session"
+  :url "https://github.com/zzwu/ring-jdbc-single-session"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.6.0"]
